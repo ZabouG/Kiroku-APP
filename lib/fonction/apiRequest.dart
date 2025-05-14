8eb2b51983868503f9d773e58392f13a278aff8c
Adrian.G <160099549+ZabouG@users.noreply.github.com>
2025-05-14 17:51:05 +0400

    Update apiRequest.dart

--- lib/fonction/apiRequest.dart
+++ lib/fonction/apiRequest.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 
 requestAPI(String query) async {
   try {
-    final response = await http.get(Uri.parse('https://api.jikan.moe/v4/manga?q=$query&genres_exclude=9,12,26,28,49,65'));
+    final response = await http.get(Uri.parse('https://api.jikan.moe/v4/manga?q=$query&genres_exclude=12,49,65'));
     if (response.statusCode == 200) {
       final jsonResponse = json.decode(response.body);
       return jsonResponse;
